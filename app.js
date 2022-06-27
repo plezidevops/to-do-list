abc3dde05991c2d80e23694718dcb8fa286d141a
Plezi Devops <plezidevops@gmail.com>
2022-06-27 09:55:47 -0400

    my to do list app

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const ejs = require('ejs');
 const app = express();
 
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+const getCurrentDay = (currentDay) => {
+  let day = '';
+  switch (currentDay) {
+    case 0:
+      day = 'Sunday';
+      break;
+    case 1:
+      day = 'Monday';
+      break;
+    case 2:
+      day = 'Tuesday';
+      break;
+    case 3:
+      day = 'Wednesday';
+      break;
+    case 4:
+      day = 'Thursday';
+      break;
+    case 5:
+      day = 'Friday';
+      break;
+    case 6:
+      day = 'Saturday';
+      break;
+    default:
+      day = `Unknown day ${currentDay}`;
+  }
+  return day;
+};
+
 app.get('/', (req, res) => {
-  res.send(`It is fun to learn!!!`);
+  const today = new Date();
+  const currentDay = getCurrentDay(today.getDay());
+  res.render('list', { day: currentDay });
 });
 
 app.listen(port, () => console.log(`Server starts on port ${port}`));
